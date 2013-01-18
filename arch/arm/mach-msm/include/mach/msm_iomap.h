741b0ef6c3f929ce9ecf01c2c1a8ef865d146ea7
Cleaneye Kim <cleaneye.kim@lge.com>
2013-01-17 22:25:35 -0500

    Add MSM_WEB register
    Change-Id: I000a710124543f82def050fd6ec86f6b42511577
    Signed-off-by: Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>

--- arch/arm/mach-msm/include/mach/msm_iomap.h
+++ arch/arm/mach-msm/include/mach/msm_iomap.h
@@ -129,6 +129,26 @@
 #include "msm_iomap-fsm9xxx.h"
 #else
 #error "Target compiled without IO map\n"
+#include "msm_iomap-7xxx.h"
+#endif
+
+#if defined(CONFIG_MACH_LGE)
+/* vibrator for MN Register */
+#define MSM_WEB_BASE          IOMEM(0xE100C000)
+#define MSM_WEB_PHYS          0xA9D00040 //0xA9D00000 in code.
+#define MSM_WEB_SIZE          SZ_4K
+#endif /* CONFIG_MACH_LGE */
+
+#if defined(CONFIG_DEBUG_MSM_UART1)
+#define MSM_DEBUG_UART_BASE	0xFB000000
+#define MSM_DEBUG_UART_PHYS	MSM_UART1_PHYS
+#elif defined(CONFIG_DEBUG_MSM_UART2)
+#define MSM_DEBUG_UART_BASE	0xFB000000
+#define MSM_DEBUG_UART_PHYS	MSM_UART2_PHYS
+#elif defined(CONFIG_DEBUG_MSM_UART3)
+#define MSM_DEBUG_UART_BASE	0xFB000000
+#define MSM_DEBUG_UART_PHYS	MSM_UART3_PHYS
+>>>>>>> 6258209... Add MSM_WEB register
 #endif
 
 #endif
