aa24053775001c6f4f6a7a7f9fbba79b25903882
Cleaneye <cleaneye@lge.com>
2013-02-06 22:13:09 -0500

    Add splint support
    Change-Id: I49d794325b71844edfdde3f0d658e415b7151325
    Signed-off-by: Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -485,12 +485,16 @@ struct task_cputime {
 #define virt_exp	utime
 #define sched_exp	sum_exec_runtime
 
+#ifdef __SPLINT__
+#define INIT_CPUTIME  NULL
+#else
 #define INIT_CPUTIME	\
 	(struct task_cputime) {					\
 		.utime = 0,					\
 		.stime = 0,					\
 		.sum_exec_runtime = 0,				\
 	}
+#endif
 
 /*
  * Disable preemption until the scheduler is running.
