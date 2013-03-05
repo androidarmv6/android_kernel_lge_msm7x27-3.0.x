1652d84c1d71d526c1d31e4fba8bb21a4fc6ef77
AndroidMeda <hephappy@gmail.com>
2013-03-05 23:48:28 +0200

    Revert "Revert "Revert "msm_fb: fixup MDP 3.00 vsync events"""
    This reverts commit 6ff33c56a835f528d37c861ca9d28f0c7aa0458d.

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -1674,14 +1674,13 @@ irqreturn_t mdp_isr(int irq, void *ptr)
 			dma = &dma2_data;
 			spin_lock_irqsave(&mdp_spin_lock, flag);
 			vsync_isr = vsync_cntrl.vsync_irq_enabled;
-			disabled_clocks = vsync_cntrl.disabled_clocks;
 			/* let's disable LCDC interrupt */
 			if (dma->waiting) {
 				dma->waiting = FALSE;
 				complete(&dma->comp);
 			}
 
-			if (!vsync_isr && !disabled_clocks) {
+			if (!vsync_isr) {
 				mdp_intr_mask &= ~LCDC_FRAME_START;
 				outp32(MDP_INTR_ENABLE, mdp_intr_mask);
 				mdp_disable_irq_nosync(MDP_VSYNC_TERM);
@@ -1691,7 +1690,7 @@ irqreturn_t mdp_isr(int irq, void *ptr)
 			}
 			spin_unlock_irqrestore(&mdp_spin_lock, flag);
 
-			if (!vsync_isr && !disabled_clocks)
+			if (!vsync_isr)
 				mdp_pipe_ctrl(MDP_CMD_BLOCK,
 					MDP_BLOCK_POWER_OFF, TRUE);
 
